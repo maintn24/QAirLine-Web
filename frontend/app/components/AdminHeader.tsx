d2e7bbb14a119adbffe4179bee802aa2fc854f21
Acerchicken <phamxuanduong1310@gmail.com>
2024-12-20 18:36:16 +0700

    Change the way saving token

--- frontend/app/components/AdminHeader.tsx
+++ frontend/app/components/AdminHeader.tsx
@@ -1,8 +1,17 @@
 'use client'
 import styles from "./styles/AdminHeader.module.css";
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+
 
 const Header = () => {
+    const router = useRouter();
+
+    // Xử lý khi nhấn Admin
+    const handleAdminClick = () => {
+        localStorage.clear(); // Xóa toàn bộ localStorage
+        router.push('/admin'); // Điều hướng đến trang admin
+    };
 
     return(
         <header className={styles.header}>
@@ -16,7 +25,11 @@ const Header = () => {
                     />
                 </Link>
             </div>
-            <div className={styles.textContainer}>
+            <div 
+                className={styles.textContainer} 
+                onClick={handleAdminClick} // Thêm sự kiện onClick vào textContainer
+                style={{ cursor: 'pointer' }} // Thêm con trỏ dạng pointer khi hover
+            >
                 <h1 className={styles.text}>Administrator</h1>
             </div>
         </header>
