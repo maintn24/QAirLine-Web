49a97447410e00e24d07410a987f6a81cc2eabb0
Mai Nguyen <maintn124@gmail.com>
2025-05-27 23:04:32 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
 };
 module.exports = {
   env: {
-    NEXT_PUBLIC_API_URL: 'https://q-air-line-web-56ot.vercel.app/api', // Trỏ đến API backend
+    NEXT_PUBLIC_API_URL: 'https://q-air-line-web-56ot.vercel.app', // Trỏ đến API backend
   },
 };
 export default nextConfig;
